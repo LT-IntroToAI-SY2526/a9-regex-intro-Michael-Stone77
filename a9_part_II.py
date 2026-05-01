d31266fb88eb7c886794758245ff91eb32ca5c2d
Michael Stone <Mstone3@cps.edu>
2026-05-01 11:33:33 -0500

    did work

--- a9_part_II.py
+++ a9_part_II.py
@@ -19,8 +19,9 @@ def get_planet_radius(planet_name: str) -> str:
         radius of the given planet
     """
     infobox_text = clean_text(get_first_infobox_text(get_page_html(planet_name)))
+    #print(infobox_text)
     # TODO: fill this in
-    pattern = "Polar radius(?P<radius>[\d.]+)"
+    pattern = "Polar Radius\n*(?P<radius>[\d.]+)"
     error_text = "Page infobox has no polar radius information"
     match = get_match(infobox_text, pattern, error_text)
     return match.group("radius")
@@ -38,7 +39,7 @@ def get_birth_date(name: str) -> str:
     infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
     # TODO: fill this in
     print(infobox_text)
-    pattern = r"Born.*?(\b(?P<birth>\d{4}-\d{2}-\d{2})\b)"
+    pattern = "(?P<birth>\d{4}-\d{2}-\d{2})"
     error_text = (
         "Page infobox has no birth information (at least none in xxxx-xx-xx format)"
     )
