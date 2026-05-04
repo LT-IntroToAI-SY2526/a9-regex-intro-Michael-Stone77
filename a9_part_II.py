5f0ffd64207a1525cd10de7f3f251e5dc4ccc1b7
Michael Stone <Mstone3@cps.edu>
2026-05-04 10:54:28 -0500

    did the part 2

--- a9_part_II.py
+++ a9_part_II.py
@@ -38,7 +38,7 @@ def get_birth_date(name: str) -> str:
     """
     infobox_text = clean_text(get_first_infobox_text(get_page_html(name)))
     # TODO: fill this in
-    print(infobox_text)
+    # print(infobox_text)
     pattern = "(?P<birth>\d{4}-\d{2}-\d{2})"
     error_text = (
         "Page infobox has no birth information (at least none in xxxx-xx-xx format)"
